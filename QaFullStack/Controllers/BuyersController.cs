a4c155b2973bf10ba1985dfed3cafd5c6721c3ef
ayodeji-shote <ayodejishote@hotmail.com>
2024-01-15 16:29:48 +0000

    Revert "Merge branch 'Develop/Backend' of https://github.com/ayodeji-shote/QAFullstack into Develop/Backend"
    This reverts commit 99aca07253ff5526de181e8b975575f46fb7c16e, reversing
    changes made to 31f95b2ae1cc63fe2cfcbc0ca1ee70a1231d472a.

--- QaFullStack/Controllers/BuyersController.cs
+++ QaFullStack/Controllers/BuyersController.cs
@@ -1,92 +1,83 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
 using QaFullStack.Model;
-
+using System.Net;
 namespace QaFullStack.Controllers
 {
-	public class BuyersController : ControllerBase
-	{
-		EstateDBContext _dBContext;
-		public BuyersController(EstateDBContext dBContext)
-		{
-			_dBContext = dBContext;
-		}
-		[HttpGet]
-		[Route("GetBuyer")]
-		// GET: BuyersController
-		public ActionResult<IEnumerable<Buyer>> Index()
-		{
-			var buyers = _dBContext.Buyers;
-			return buyers;
-		}
+    public class BuyersController : ControllerBase
+    {
+        EstateDBContext _dBContext;
+        public BuyersController(EstateDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        [HttpGet]
+        [Route("GetBuyers")]
+        // GET: BuyersController
+        public ActionResult<IEnumerable<Buyer>> Index()
+        {
+            var buyers = _dBContext.Buyers;
+            return buyers;
+        }
 
-		//// GET: BuyersController/Details/5
-		//public ActionResult Details(int id)
-		//{
-		//    return ();
-		//}
+        // GET: BuyersController/Details/5
+        [HttpGet]
+        [Route("GetBuyer/{id}")]
+        public ActionResult<Buyer> Details(int id)
+        {
+            try
+            {
+                var buyer = _dBContext.Buyers.Find(id);
+                return buyer;
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
 
-		//// GET: BuyersController/Create
-		//public ActionResult Create()
-		//{
-		//    return ();
-		//}
+        // POST: BuyersController/Create
+        [HttpPost]
+        [Route("CreateBuyer")]
+        public ActionResult<Buyer> Create(Buyer bnbuyer)
+        {
+           _dBContext.Buyers.Add(bnbuyer);
+            _dBContext.SaveChanges();
+            return CreatedAtAction(nameof(Details), new { id = bnbuyer.Id }, bnbuyer);
+        }
 
-		//// POST: BuyersController/Create
-		//[HttpPost]
-		//[ValidateAntiForgeryToken]
-		//public ActionResult Create(IFormCollection collection)
-		//{
-		//    try
-		//    {
-		//        return RedirectToAction(nameof(Index));
-		//    }
-		//    catch
-		//    {
-		//        return ();
-		//    }
-		//}
 
-		//// GET: BuyersController/Edit/5
-		//public ActionResult Edit(int id)
-		//{
-		//    return ();
-		//}
 
-		//// POST: BuyersController/Edit/5
-		//[HttpPost]
-		//[ValidateAntiForgeryToken]
-		//public ActionResult Edit(int id, IFormCollection collection)
-		//{
-		//    try
-		//    {
-		//        return RedirectToAction(nameof(Index));
-		//    }
-		//    catch
-		//    {
-		//        return ();
-		//    }
-		//}
+        // POST: BuyersController/Edit/5
+        [HttpPut]
+        [Route("EditBuyer/{id}")]
+        public ActionResult<Buyer> Edit(int id , Buyer buyer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dBContext.Entry(buyer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _dBContext.SaveChanges();
+                return new JsonResult("buyer updated successfully");
+            }
+            return BadRequest(ModelState);
 
-		//// GET: BuyersController/Delete/5
-		//public ActionResult Delete(int id)
-		//{
-		//    return ();
-		//}
 
-		//// POST: BuyersController/Delete/5
-		//[HttpPost]
-		//[ValidateAntiForgeryToken]
-		//public ActionResult Delete(int id, IFormCollection collection)
-		//{
-		//    try
-		//    {
-		//        return RedirectToAction(nameof(Index));
-		//    }
-		//    catch
-		//    {
-		//        return ();
-		//    }
-		//}
-	}
+        }
+
+
+
+        // POST: BuyersController/Delete/5
+        [HttpDelete]
+        [Route("DeleteBuyer/{id}")]
+        public HttpStatusCode Delete(int id)
+        {
+            var Buyer = _dBContext.Buyers.Find(id);
+            if (Buyer == null)
+                return HttpStatusCode.NotFound;
+            _dBContext.Remove(Buyer);
+            _dBContext.SaveChanges();
+            return HttpStatusCode.NoContent;
+        }
+    }
 }
