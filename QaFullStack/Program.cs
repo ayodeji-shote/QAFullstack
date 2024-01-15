ec121d4b8fc53ec4e857893a56b7919d17583d9f
Piotr Ryba <piotr.ryba42@gmail.com>
2024-01-15 14:37:23 +0000

    Update Program.cs

--- QaFullStack/Program.cs
+++ QaFullStack/Program.cs
@@ -6,32 +6,21 @@ using QaFullStack.Model;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-<<<<<<< HEAD
 
-builder.Services.AddControllers();
-
-=======
 builder.Services.AddDbContext<EstateDBContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("EstateAgentAppCon")));
->>>>>>> 828845f87a793e21ec23fd987b215355c3077c54
+
 // json serializer
 builder.Services.AddControllers().AddNewtonsoftJson(options=>
 options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options=>options.SerializerSettings.ContractResolver=new Newtonsoft.Json.Serialization.DefaultContractResolver());
 
-<<<<<<< HEAD
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-=======
->>>>>>> 828845f87a793e21ec23fd987b215355c3077c54
 var app = builder.Build();
-
 
 app.UseCors(c=>c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 // Configure the HTTP request pipeline.
