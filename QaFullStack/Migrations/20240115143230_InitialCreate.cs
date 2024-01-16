9a54831018fe596c5381161aefd3c2f0faa29cad
ChengyuKang <80816430+ChengyuKang@users.noreply.github.com>
2024-01-16 10:55:19 +0000

    property changes made
    property changes

---
+++ QaFullStack/Migrations/20240115143230_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QaFullStack.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BOOKING_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BUYER_ID = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    PROPERTY_ID = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    TIME = table.Column<string>(type: "nvarchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BOOKING_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Buyers",
+                columns: table => new
+                {
+                    BUYER_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FIRST_NAME = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    SURNAME = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    ADDRESS = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    POSTCODE = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    PHONE = table.Column<string>(type: "nvarchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buyers", x => x.BUYER_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    PROPERTY_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ADDRESS = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    POSTCODE = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    TYPE = table.Column<string>(type: "nvarchar(9)", nullable: false),
+                    NUMBER_OF_BEDROOMS = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    NUMBER_OF_BATHROOMS = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    GARDEN = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    PRICE = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    STATUS = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    SELLER_ID = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    BUYER_ID = table.Column<string>(type: "nvarchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.PROPERTY_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    SELLER_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FIRST_NAME = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    SURNAME = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    ADDRESS = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    POSTCODE = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    PHONE = table.Column<string>(type: "nvarchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.SELLER_ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Buyers");
+
+            migrationBuilder.DropTable(
+                name: "Properties");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+        }
+    }
+}
---
+++ QaFullStack/Migrations/20240115152932_CreateDBEstate2.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QaFullStack.Migrations
+{
+    public partial class CreateDBEstate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BUYER_ID",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "GARDEN",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "NUMBER_OF_BATHROOMS",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "NUMBER_OF_BEDROOMS",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "POSTCODE",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "PRICE",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "SELLER_ID",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "STATUS",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "TYPE",
+                table: "Properties");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BUYER_ID",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GARDEN",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NUMBER_OF_BATHROOMS",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NUMBER_OF_BEDROOMS",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "POSTCODE",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PRICE",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SELLER_ID",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "STATUS",
+                table: "Properties",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TYPE",
+                table: "Properties",
+                type: "nvarchar(9)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
