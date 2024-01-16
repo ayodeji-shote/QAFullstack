c1da6e4f71357d490f6ce0ad58c12ff45f84101f
ChengyuKang <80816430+ChengyuKang@users.noreply.github.com>
2024-01-16 14:15:45 +0000

    Update IRepositoryBase.cs

--- QaFullStack/Repositories/IRepositoryBase.cs
+++ QaFullStack/Repositories/IRepositoryBase.cs
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using QaFullStack.Model;
 using System.Linq.Expressions;
 namespace QaFullStack.Repositories
 {
@@ -24,25 +17,4 @@ namespace QaFullStack.Repositories
         T Update(T entity);
         void Delete(T entity);
     }
-=======
-﻿using System.Linq.Expressions;
-namespace QaFullStack.Repositories
-{
-	// This is the interface for the RepositoryBase class
-	// It is used to define the methods that will be used in the RepositoryBase class
-	// The RepositoryBase class is used to define the methods that will be used in the Repository classes
-	// The Repository classes are used to define the methods that will be used in the Controllers
-	// The Controllers are used to define the methods that will be used in the API
-	// The API is used to define the methods that will be used in the Front End
-	// The Front End is used to define the methods that will be used by the user
-	public interface IRepositoryBase<T> where T : class
-	{
-		IEnumerable<T> FindAll();
-		IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
-		T FindById(int id);
-		T Create(T entity);
-		T Update(T entity);
-		void Delete(T entity);
-	}
->>>>>>> 61907817172a741ace3454c1c9aca50b4db9dfe0
 }
